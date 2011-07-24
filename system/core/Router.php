21b741183e80d6ef6bf0bde3694f73e583b4b6bf
Joe Auty <joe@mixolydian.netmusician.org>
2011-07-24 16:31:04 -0400

    rewrite patch
    Signed-off-by: Joe Auty <root@mixolydian.netmusician.org>

--- system/core/Router.php
+++ system/core/Router.php
@@ -231,6 +231,9 @@ class CI_Router {
 		{
 			return $segments;
 		}
+		
+		/* NetMusician hack */
+		if (defined('DISABLE_CIROUTING')) { return; }
 
 		// Is the controller in a sub-folder?
 		if (is_dir(APPPATH.'controllers/'.$segments[0]))
